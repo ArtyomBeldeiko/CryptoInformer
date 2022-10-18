2b39c3859c8ae4322ad6e302e3a6417886e1125b
ArtyomBeldeiko <artyom.beldeiko@gmail.com>
2022-10-18 23:09:57 +0300

    Initial Commit

---
+++ CryptoInformer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoInformer
+//
+//  Created by Artyom Beldeiko on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
